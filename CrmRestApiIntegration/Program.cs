e4c72a021d5303ae437ecfea3c7d212844e98265
Tony Kajtazi <TKajtazi@fadev.local>
2018-08-08 12:05:46 +0100

    Add project files.

---
+++ CrmRestApiIntegration/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CrmRestApiIntegration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseKestrel(options =>
+                    {
+                        options.Limits.MaxConcurrentConnections = 100;
+                    }
+
+                );
+    }
+}
