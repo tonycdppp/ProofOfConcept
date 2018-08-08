6553fe2bca983d1f733322957ed0b30e7ddff321
tkajtazi <kajtazi@gmail.com>
2018-08-08 15:36:33 +0100

    new clean project

--- CrmRestApiIntegration/DeployToIISFolder.bat
+++
@@ -1,6 +0,0 @@
-﻿TaskKill /F /IM dotnet.exe
-iisreset /stop
-cd C:\DEVELOPMENT\Ticketing\AV7\Source\Api\TheFA.Ticketing.CoreApi
-dotnet publish --framework netcoreapp1.1 --output "c:\PUBLISHED\CoreApi" --configuration Debug
-iisreset /start
-pause  
